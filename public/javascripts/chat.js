1812f245522330a6dfa856bb870e8c675f0b3693
leemingoo <dwmapi@naver.com>
2023-10-26 16:28:01 +0900

    style: 버튼 클릭 시 li 추가 테스트

---
+++ public/javascripts/chat.js
@@ -0,0 +1,17 @@
+const boxChatLog = document.getElementById('box-Chat-Log');
+const txtInput = document.getElementById('txt-Input');
+const btnSubmit = document.getElementById('btn-Submit');
+
+const scrollToBottom = () => {
+  boxChatLog.scrollTop = chatLog.scrollHeight;
+}
+
+// 예를 들어, 새 메시지를 추가할 때 아래와 같이 스크롤 함수를 호출
+const addNewMessage = (message) => {
+  let newMessage = document.createElement("li");
+  newMessage.textContent = message;
+  boxChatLog.appendChild(newMessage);
+  scrollToBottom();
+}
+
+const a = document.getElementById('a');
