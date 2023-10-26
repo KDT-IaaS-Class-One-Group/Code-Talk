a8d46574b041ee0c7dba2f610dc4ed3242c1c7bd
Tech1997 <dev.honing@gmail.com>
2023-10-26 17:22:46 +0900

    보낸 메시지의 내용을 배열에 동적으로 추가완료

--- app.js
+++ app.js
@@ -3,32 +3,39 @@ const socketIo = require('socket.io');
 const http = require('http');
 
 const app = express();
-const port = 8080; // 사용할 포트 번호
+const port = 8080;
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// 정적 파일 제공 (HTML, CSS, JavaScript 등을 저장하는 디렉토리 지정)
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true } )); // URL-encoded 데이터 파싱 미들웨어 추가
+let messages = []; // 메시지를 저장할 배열
 
-// GET 요청 처리
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-// POST 요청 처리
 app.post('/send', (req, res) => {
   const message = req.body.text;
   console.log('Received message:', message);
+  // 메시지를 배열에 추가
+  messages.push(message);
+  // 콘솔에 추가된 메시지 출력
+  console.log(`배열에 "${message}" 요소가 추가되었습니다`);
 
-  // 클라이언트에게 메시지를 보냄
+  // 메시지를 기록하고 클라이언트에게 브로드캐스트
+  messages.push(message);
   io.emit('chat message', message);
 
-  // 응답을 보낸 후, 추가 응답을 보내지 않음
   res.status(200).send('Message sent');
 });
 
-// 서버 시작
+app.get('/messages', (req, res) => {
+  // 클라이언트에서 메시지를 조회하는 요청을 처리
+  res.json(messages);
+});
+
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
@@ -36,11 +43,11 @@ server.listen(port, () => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // 클라이언트로부터 메시지 수신
   socket.on('chat message', (msg) => {
     console.log('Received message:', msg);
+    messages.push(msg); // 메시지를 기록
+    console.log(messages);
 
-    // 클라이언트에게 메시지 브로드캐스트
     io.emit('chat message', msg);
   });
 
@@ -48,3 +55,13 @@ io.on('connection', (socket) => {
     console.log('User disconnected');
   });
 });
+// 서버에서 메시지 배열에 메시지 추가
+app.post('/send', (req, res) => {
+  const message = req.body.text;
+
+  // 클라이언트에게 메시지 브로드캐스트
+  io.emit('chat message', message);
+  
+  res.status(200).send('Message sent');
+});
+
