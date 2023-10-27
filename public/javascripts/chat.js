f065a73a42679f0b9238516e5b837ea8841ab08f
leemingoo <dwmapi@naver.com>
2023-10-27 15:34:18 +0900

    refactor: class, id 이름 직관화

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,54 +1,23 @@
-const messageInput = document.getElementById("message-input");
-const sendButton = document.getElementById("send-button");
-const chatMessages = document.getElementById("chat-messages");
+const txtContent = document.getElementById("txt-Content");
+const btnSend = document.getElementById("btn-Send");
+const outputChat = document.getElementById("output-Chat");
 
-sendButton.addEventListener("click", sendMessage);
+btnSend.addEventListener("click", sendMessage);
 
 function sendMessage() {
-  const messageText = messageInput.value;
+  const messageText = txtContent.value;
   if (messageText.trim() !== "") {
-    const messageElement = document.createElement("li");
+    const messageElement = document.createElement("li"); // Change to <li>
     messageElement.textContent = messageText;
-    chatMessages.appendChild(messageElement);
-    messageInput.value = "";
+    outputChat.appendChild(messageElement);
+    txtContent.value = "";
 
-    // Scroll to the latest message
-    chatMessages.scrollTop = chatMessages.scrollHeight;
+    outputChat.scrollTop = outputChat.scrollHeight;
   }
 }
 
-messageInput.addEventListener("keyup", function(event) {
+txtContent.addEventListener("keyup", function(event) {
   if (event.key === "Enter") {
     sendMessage();
   }
 });
-
-function sendMessage(isOwnMessage) {
-  const messageText = messageInput.value;
-  if (messageText.trim() !== "") {
-    const messageElement = document.createElement("li");
-    messageElement.textContent = messageText;
-
-    if (isOwnMessage) {
-      messageElement.classList.add("own-message"); // Add a class for own messages
-    }
-
-    chatMessages.appendChild(messageElement);
-    messageInput.value = "";
-
-    // Scroll to the latest message
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-  }
-}
-
-// When sending your own message
-sendButton.addEventListener("click", () => {
-  sendMessage(true);
-});
-
-// When receiving a message (simulate a received message)
-function receiveMessage() {
-  const messageText = "This is a received message.";
-  sendMessage(false);
-}
-
