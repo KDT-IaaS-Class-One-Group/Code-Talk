cf52f6907e993cc409c4a34c24d57655e19ee377
leemingoo <dwmapi@naver.com>
2023-10-27 16:10:41 +0900

    style: form 태그 추가

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -8,26 +8,26 @@ function sendMessage() {
   const messageText = txtContent.value;
   if (messageText.trim() !== "") {
     const messageElement = document.createElement("li");
-    
+
     // 현재 날짜와 시간을 생성
     const currentDate = new Date();
     const timestamp = currentDate.toLocaleString();
-    
+
     // 메시지와 날짜/시간을 각각 <span> 요소로 분리
     const messageSpan = document.createElement("span");
     messageSpan.textContent = messageText;
-    
+
     const timestampSpan = document.createElement("span");
     timestampSpan.textContent = timestamp;
-    
+
     // 각각의 <span> 요소를 <li>에 추가
     messageElement.appendChild(messageSpan);
     messageElement.appendChild(timestampSpan);
-    
+
     // CSS 클래스를 추가하여 스타일링 가능
     messageSpan.classList.add("message-text");
     timestampSpan.classList.add("timestamp");
-    
+
     outputChat.appendChild(messageElement);
     txtContent.value = "";
 
