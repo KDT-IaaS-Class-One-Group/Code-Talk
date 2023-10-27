8e847f34769528478c3850b79c041f653d3cc836
leemingoo <dwmapi@naver.com>
2023-10-27 16:23:24 +0900

    feat: socket.io 추가

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const port = 8080;
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const port = process.env.port || 8080;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve HTML file
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
+// Socket.io connection
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message);
+  });
+});
+
+server.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
