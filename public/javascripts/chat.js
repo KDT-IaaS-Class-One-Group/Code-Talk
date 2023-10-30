9588a4495164b50d504cf88ae1b276cef1bb8b37
leemingoo <dwmapi@naver.com>
2023-10-30 15:00:14 +0900

    fix: GET 요청 처리를 위한 초기화

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -26,17 +26,6 @@ function sendMessage() {
     outputChat.scrollTop = outputChat.scrollHeight;
     txtContent.value = "";
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", `/sendMessage?message=${messageText}`, true);
-    xhr.send();
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        // 서버 응답을 처리하고 싶은 경우 여기에 작성
-        // 여기에서 서버로부터 받은 응답 데이터를 처리할 수 있습니다.
-        console.log("서버로부터 받은 응답:", xhr.responseText);
-      }
-    };
   }
 }
 
