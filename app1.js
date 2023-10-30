6d0585d5c4269ae0be2ec5e828f2cbe4fde323e5
Tech1997 <dev.honing@gmail.com>
2023-10-30 12:01:03 +0900

    요소를 추가하고 새로운 파일을 동적으로 생성하는 로직 작성

--- app1.js
+++ app1.js
@@ -1,37 +1,38 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const fs = require('fs');
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 const port = 8080;
 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
 
-  // 클라이언트로부터 메시지를 받음
-  socket.on('chat message', (msg) => {
-    // 메시지를 다른 클라이언트에게 브로드캐스트
-    io.emit('chat message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
+// POST 요청을 처리
 app.post('/update', (req, res) => {
   const newText = req.body.text; // POST 요청에서 전송된 텍스트 데이터
 
-  // 메시지를 클라이언트로 브로드캐스트
-  io.emit('chat message', newText);
+  // 기존 index1.html 파일을 읽어온다
+  fs.readFile(__dirname + '/public/index1.html', 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading file');
+    }
 
-  res.status(200).send('Message sent');
+    // 새로운 내용을 기존 내용에 추가
+    const updatedHtml = data.replace('</ul>', `  <li>${newText}</li>\n</ul>`);
+
+    // 새로운 index1.html 파일을 생성
+    fs.writeFile(__dirname + '/public/index1.html', updatedHtml, 'utf8', (err) => {
+      if (err) {
+        return res.status(500).send('Error writing file');
+      }
+
+      res.status(200).send('File updated successfully');
+    });
+  });
 });
 
-server.listen(port, () => {
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
