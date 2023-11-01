d0fe87fa78ade830e381ee2c2a1af69013409cba
leemingoo <dwmapi@naver.com>
2023-11-01 14:47:11 +0900

    feat: 채팅 메시지 이벤트를 수신하기 위한 리스너 추가 받은 메시지를 모든 연결된 클라이언트에 브로드캐스트

--- app.js
+++ app.js
@@ -6,40 +6,29 @@ const app = express();
 const { Server } = require('socket.io');
 const port = 8080;
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-// Serve HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Socket.io connection
+const io = new Server(server);
+
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('사용자가 연결되었습니다.');
+
+  // 채팅 메시지 이벤트를 수신하기 위한 리스너
+  socket.on('chat message', (msg) => {
+    // 받은 메시지를 모든 연결된 클라이언트에 브로드캐스트합니다.
+    io.emit('chat message', { message: msg, timestamp: new Date().toLocaleString() });
+  });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-
-  socket.on('chat message', (message) => {
-    io.emit('chat message', message);
+    console.log('사용자가 연결을 해제했습니다.');
   });
 });
 
-app.use(express.static("public"));
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.post("/sendMessage", (req, res) => {
-  const message = req.body.message;
-  res.json({ message });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 });
