8b02bb8fe0217ef04bfe8f498b712e95e9e9dfa8
leemingoo <dwmapi@naver.com>
2023-11-01 14:47:44 +0900

    fix: 간단 오타 수정

--- app.js
+++ app.js
@@ -17,10 +17,8 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
   console.log('사용자가 연결되었습니다.');
-
-  // 채팅 메시지 이벤트를 수신하기 위한 리스너
+  
   socket.on('chat message', (msg) => {
-    // 받은 메시지를 모든 연결된 클라이언트에 브로드캐스트합니다.
     io.emit('chat message', { message: msg, timestamp: new Date().toLocaleString() });
   });
 
