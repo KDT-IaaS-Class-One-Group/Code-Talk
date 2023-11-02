401f3ad33eae66dc316825dde47fedc498e042e0
Ho Nyeong <dev.honing@gmail.com>
2023-11-02 18:39:24 +0900

    test:
    undefined가 중복으로 작성돼서 테스트 커밋

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,3 +1,4 @@
+// 파일 이름: chat.js
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io();
 
@@ -15,6 +16,22 @@ document.addEventListener("DOMContentLoaded", () => {
     if (messageText.trim() !== "") {
       txtContent.value = "";
 
+      const messageElement = document.createElement("li");
+      const messageSpan = document.createElement("span");
+      const timestampSpan = document.createElement("span");
+
+      messageSpan.textContent = `${socket.nickname}: ${messageText}`;
+      timestampSpan.textContent = new Date().toLocaleString();
+
+      messageElement.appendChild(messageSpan);
+      messageElement.appendChild(timestampSpan);
+
+      messageSpan.classList.add("message-text");
+      timestampSpan.classList.add("timestamp");
+
+      outputChat.appendChild(messageElement);
+      outputChat.scrollTop = outputChat.scrollHeight;
+
       sendMessageToServer(messageText);
     }
   }
@@ -30,17 +47,42 @@ document.addEventListener("DOMContentLoaded", () => {
     const messageElement = document.createElement("li");
     const messageSpan = document.createElement("span");
     const timestampSpan = document.createElement("span");
-
-    messageSpan.textContent = msg.message;
-    timestampSpan.textContent = msg.timestamp;
-
-    messageElement.appendChild(messageSpan);
-    messageElement.appendChild(timestampSpan);
-
-    messageSpan.classList.add("message-text");
-    timestampSpan.classList.add("timestamp");
-
+  
+    // 정규표현식을 사용하여 사용자 이름과 메시지 내용 추출
+    const regex = /^(.*?):\s(.*)$/;
+    const matches = msg.message.match(regex);
+  
+    if (matches && matches.length === 3) {
+      const userName = matches[1]; // 첫 번째 그룹: 사용자 이름
+      const messageText = matches[2]; // 두 번째 그룹: 메시지 내용
+  
+      // 사용자 이름, 메시지 내용, 타임스탬프를 각각의 <span>에 넣어줌
+      messageSpan.textContent = messageText;
+      timestampSpan.textContent = msg.timestamp;
+  
+      // 사용자 이름을 표시하는 <span> 태그를 생성하고 스타일을 추가
+      const userIdSpan = document.createElement("span");
+      userIdSpan.textContent = userName;
+      userIdSpan.classList.add("user-id");
+  
+      // <li> 태그에 각각의 <span> 태그를 추가
+      messageElement.appendChild(userIdSpan);
+      messageElement.appendChild(messageSpan);
+      messageElement.appendChild(timestampSpan);
+      
+      // 스타일 클래스 추가
+      messageSpan.classList.add("message-text");
+      timestampSpan.classList.add("timestamp");
+    } else {
+      // 매치되지 않는 경우 기본적으로 전체 메시지를 표시
+      messageSpan.textContent = msg.message;
+      messageElement.appendChild(messageSpan);
+      messageElement.appendChild(timestampSpan);
+      messageSpan.classList.add("message-text");
+      timestampSpan.classList.add("timestamp");
+    }
+  
     outputChat.appendChild(messageElement);
     outputChat.scrollTop = outputChat.scrollHeight;
   });
-});
+  });
