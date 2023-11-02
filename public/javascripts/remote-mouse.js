526e5be612a5391429f1249005648b1c5bb39260
leemingoo <dwmapi@naver.com>
2023-11-02 19:27:47 +0900

    style: 클릭 시 주변 원 생기는 이펙트

--- public/javascripts/remote-mouse.js
+++ public/javascripts/remote-mouse.js
@@ -5,25 +5,38 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('mousemove', (event) => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
-    
-    // 사용자마다 무작위 색상 생성
-    const userColor = getRandomColor();
 
-    socket.emit('mousePosition', { x: mouseX, y: mouseY, color: userColor });
+     socket.emit('mousePosition', { x: mouseX, y: mouseY });
+  });
+
+  document.addEventListener('click', (event) => {
+    const clickEffect = document.createElement('div');
+    clickEffect.className = 'click-effect';
+    
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    clickEffect.style.left = `${x}px`;
+    clickEffect.style.top = `${y}px`;
+    
+    remoteMousesElement.appendChild(clickEffect);
+
+    // 클릭 효과가 서서히 사라지도록 설정 (예: 0.5초 후 삭제)
+    setTimeout(() => {
+      clickEffect.remove();
+    }, 500); // 0.5초 (500ms)
   });
 
   socket.on('remoteMousesPosition', (remoteMouses) => {
     remoteMousesElement.innerHTML = '';
     for (let id in remoteMouses) {
-      const userColor = remoteMouses[id].color;
-      const isCurrentUser = id === socket.id;
-
-      const pointer = document.createElement('div');
-      pointer.className = 'remote-mouse-pointer';
-      pointer.style.left = remoteMouses[id].x + 'px';
-      pointer.style.top = remoteMouses[id].y + 'px';
-      pointer.style.backgroundColor = isCurrentUser ? 'transparent' : userColor;
-      remoteMousesElement.appendChild(pointer);
+      if (id !== socket.id) { // 현재 사용자의 마우스를 숨깁니다
+        const pointer = document.createElement('div');
+        pointer.className = 'remote-mouse-pointer';
+        pointer.style.left = remoteMouses[id].x + 'px';
+        pointer.style.top = remoteMouses[id].y + 'px';
+        remoteMousesElement.appendChild(pointer);
+      }
     }
   });
 
@@ -33,8 +46,4 @@ document.addEventListener('DOMContentLoaded', () => {
       disconnectedPointer.remove();
     }
   });
-
-  function getRandomColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
 });
