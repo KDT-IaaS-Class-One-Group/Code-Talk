96962fa63bd3faedff07933c95c8028e0d6f0cfa
Ho Nyeong <dev.honing@gmail.com>
2023-11-02 19:28:28 +0900

    style:
    insertBefore() 메서드로 요소를 아래쪽부터 쌓도록 구현 완료

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,8 +1,34 @@
-// 파일 이름: chat.js
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io();
-
   const outputChat = document.getElementById("output-Chat");
+
+  function createMessageElement(msg) {
+    const messageElement = document.createElement("li");
+    const messageSpan = document.createElement("span");
+    const timestampSpan = document.createElement("span");
+    const userIdSpan = document.createElement("span");
+
+    messageSpan.textContent = msg.message;
+    timestampSpan.textContent = msg.timestamp;
+    userIdSpan.textContent = msg.userId;
+
+    messageSpan.classList.add("message-text");
+    timestampSpan.classList.add("timestamp");
+    userIdSpan.classList.add("user-id");
+
+    messageElement.appendChild(userIdSpan);
+    messageElement.appendChild(messageSpan);
+    messageElement.appendChild(timestampSpan);
+
+    return messageElement;
+  }
+
+  socket.on('chat message', (msg) => {
+    const messageElement = createMessageElement(msg);
+    outputChat.insertBefore(messageElement, outputChat.firstChild);
+    outputChat.scrollTop = outputChat.scrollHeight;
+  });
+
   const txtContent = document.getElementById("txt-Content");
   const btnSend = document.getElementById("btn-Send");
 
@@ -15,23 +41,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (messageText.trim() !== "") {
       txtContent.value = "";
-
-      const messageElement = document.createElement("li");
-      const messageSpan = document.createElement("span");
-      const timestampSpan = document.createElement("span");
-
-      messageSpan.textContent = `${socket.nickname}: ${messageText}`;
-      timestampSpan.textContent = new Date().toLocaleString();
-
-      messageElement.appendChild(messageSpan);
-      messageElement.appendChild(timestampSpan);
-
-      messageSpan.classList.add("message-text");
-      timestampSpan.classList.add("timestamp");
-
-      outputChat.appendChild(messageElement);
-      outputChat.scrollTop = outputChat.scrollHeight;
-
       sendMessageToServer(messageText);
     }
   }
@@ -42,47 +51,4 @@ document.addEventListener("DOMContentLoaded", () => {
       sendMessage();
     }
   });
-
-  socket.on('chat message', (msg) => {
-    const messageElement = document.createElement("li");
-    const messageSpan = document.createElement("span");
-    const timestampSpan = document.createElement("span");
-  
-    // 정규표현식을 사용하여 사용자 이름과 메시지 내용 추출
-    const regex = /^(.*?):\s(.*)$/;
-    const matches = msg.message.match(regex);
-  
-    if (matches && matches.length === 3) {
-      const userName = matches[1]; // 첫 번째 그룹: 사용자 이름
-      const messageText = matches[2]; // 두 번째 그룹: 메시지 내용
-  
-      // 사용자 이름, 메시지 내용, 타임스탬프를 각각의 <span>에 넣어줌
-      messageSpan.textContent = messageText;
-      timestampSpan.textContent = msg.timestamp;
-  
-      // 사용자 이름을 표시하는 <span> 태그를 생성하고 스타일을 추가
-      const userIdSpan = document.createElement("span");
-      userIdSpan.textContent = userName;
-      userIdSpan.classList.add("user-id");
-  
-      // <li> 태그에 각각의 <span> 태그를 추가
-      messageElement.appendChild(userIdSpan);
-      messageElement.appendChild(messageSpan);
-      messageElement.appendChild(timestampSpan);
-      
-      // 스타일 클래스 추가
-      messageSpan.classList.add("message-text");
-      timestampSpan.classList.add("timestamp");
-    } else {
-      // 매치되지 않는 경우 기본적으로 전체 메시지를 표시
-      messageSpan.textContent = msg.message;
-      messageElement.appendChild(messageSpan);
-      messageElement.appendChild(timestampSpan);
-      messageSpan.classList.add("message-text");
-      timestampSpan.classList.add("timestamp");
-    }
-  
-    outputChat.appendChild(messageElement);
-    outputChat.scrollTop = outputChat.scrollHeight;
-  });
-  });
+});
