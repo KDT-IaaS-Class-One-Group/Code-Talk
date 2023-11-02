bd2c839773df3d6bcaebd1c44e900bd52b05ee23
Ho Nyeong <dev.honing@gmail.com>
2023-11-02 20:20:32 +0900

    style: user-id가 메시지 안에 출력되는 문제 발생

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io();
   const outputChat = document.getElementById("output-Chat");
+  const txtContent = document.getElementById("txt-Content");
+  const btnSend = document.getElementById("btn-Send");
 
   function createMessageElement(msg) {
     const messageElement = document.createElement("li");
@@ -26,11 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
   socket.on('chat message', (msg) => {
     const messageElement = createMessageElement(msg);
     outputChat.insertBefore(messageElement, outputChat.firstChild);
-    outputChat.scrollTop = outputChat.scrollHeight;
   });
-
-  const txtContent = document.getElementById("txt-Content");
-  const btnSend = document.getElementById("btn-Send");
 
   function sendMessageToServer(message) {
     socket.emit('chat message', message);
@@ -41,46 +39,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (messageText.trim() !== "") {
       txtContent.value = "";
+      const message = `${socket.nickname}: ${messageText}`;
+      const timestamp = new Date().toLocaleString();
+      const messageElement = createMessageElement({
+        message: message,
+        userId: socket.nickname,
+        timestamp: timestamp
+      });
 
-      const messageElement = createMessageElement(`${socket.nickname}: ${messageText}`, new Date().toLocaleString());
-
-      outputChat.appendChild(messageElement);
-      outputChat.scrollTop = outputChat.scrollHeight;
-
+      outputChat.insertBefore(messageElement, outputChat.firstChild);
       sendMessageToServer(messageText);
     }
-  }
-
-  function createMessageElement(message, timestamp) {
-    const messageElement = document.createElement("li");
-    const userIdSpan = document.createElement("span");
-    const messageSpan = document.createElement("span");
-    const timestampSpan = document.createElement("span");
-
-    const regex = /^(.*?):\s(.*)$/;
-    const matches = message.match(regex);
-
-    if (matches && matches.length === 3) {
-      const userName = matches[1];
-      const messageText = matches[2];
-
-      userIdSpan.textContent = userName;
-      userIdSpan.classList.add("user-id");
-      messageSpan.textContent = messageText;
-    } else {
-      messageSpan.textContent = message;
-    }
-
-    timestampSpan.textContent = timestamp;
-
-    messageSpan.classList.add("message-text");
-    timestampSpan.classList.add("timestamp");
-
-    messageElement.appendChild(userIdSpan);
-    messageElement.appendChild(messageSpan);
-    messageElement.appendChild(timestampSpan);
-
-    return messageElement;
   }
 
   btnSend.addEventListener("click", sendMessage);
@@ -88,11 +57,5 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.key === "Enter") {
       sendMessage();
     }
-  });
-
-  socket.on('chat message', (msg) => {
-    const messageElement = createMessageElement(msg.message, msg.timestamp);
-    outputChat.appendChild(messageElement);
-    outputChat.scrollTop = outputChat.scrollHeight;
   });
 });
