e03aace3851abe084c8e417f52ff145c20741add
leemingoo <dwmapi@naver.com>
2023-11-02 16:35:59 +0900

    bug: 내 마우스의 좌표와 상대방의 마우스 좌표가 다른 버그 발생

--- public/javascripts/remote-mouse.js
+++ public/javascripts/remote-mouse.js
@@ -1,28 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
   const socket = io();
-  let isMouseOut = false;
+
+  const root = document.getElementById('root');
+  const remoteMouse = document.getElementById('remote-mouse');
 
   document.addEventListener('mousemove', (event) => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
-    if (!isMouseOut) {
+    // Check if mouse is within the root element
+    const rect = root.getBoundingClientRect();
+    const isInRoot = mouseX >= rect.left && mouseX <= rect.right && mouseY >= rect.top && mouseY <= rect.bottom;
+
+    if (isInRoot) {
       socket.emit('mousePosition', { x: mouseX, y: mouseY });
     }
-  });
-
-  document.addEventListener('mouseout', () => {
-    isMouseOut = true;
-  });
-
-  document.addEventListener('mouseover', () => {
-    isMouseOut = false;
   });
 
   socket.on('remoteMousePosition', (data) => {
     const remoteMousePointer = document.getElementById('remote-mouse-pointer');
     const { x, y } = data;
+
     remoteMousePointer.style.left = `${x}px`;
     remoteMousePointer.style.top = `${y}px`;
+  });
+
+  // Prevent scrolling when mouse is outside root element
+  root.addEventListener('mouseleave', () => {
+    document.body.style.overflow = 'hidden';
+  });
+
+  root.addEventListener('mouseenter', () => {
+    document.body.style.overflow = 'auto';
   });
 });
